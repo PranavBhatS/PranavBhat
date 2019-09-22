4f4d67c27523ac7a130a06137d796d1d4efe43ff
PranavBhatS <pranavsarang@gmail.com>
2019-09-22 10:24:26 +0530

    Updates

---
+++ precache-manifest.962877f50d37f3f8dd3771a054fbe374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "75473a14e7b079aebd13",
+    "url": "/me/static/js/main.75473a14.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "75473a14e7b079aebd13",
+    "url": "/me/static/css/main.38c12e76.chunk.css"
+  },
+  {
+    "revision": "0f93b72662e8b21a7ce00776f4b8cb9f",
+    "url": "/me/index.html"
+  }
+];
